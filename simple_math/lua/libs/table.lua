0623af5f29ad35e0f448726690515e1c9a63cc30
pockets <33330490+pocketssss@users.noreply.github.com>
2023-02-19 05:20:08 +0300

    Update table.lua

--- simple_math/lua/libs/table.lua
+++ simple_math/lua/libs/table.lua
@@ -1,28 +1,33 @@
 local getkeys, rand = table.GetKeys, math.random		 -- Some warmup(???)
 
-function table.Count(t)				 -- Should be faster
-	local out = #getkeys(t)
-	return out
+function table.Count(t)
+    local count = 0
+    for _ in pairs(t) do count = count + 1 end
+    return count
 end
 
-function table.Empty(t)				 -- Indisputably faster.
-	t = nil
-	t = {}
+function table.Empty(t)
+    for k in pairs(t) do t[k] = nil end
 end
 
-function table.Random(t) 			 -- There are no loops or conditions. Should be faster.
-	local keyset = getkeys(t)
-	local k = keyset[rand(1, #keyset)]
-	return t[k]
+function table.Random(t)
+    local n = table.Count(t)
+    if n == 0 then return nil end
+    local rand_index = rand(1, n)
+    local i = 1
+    for _, v in pairs(t) do
+        if i == rand_index then return v end
+        i = i + 1
+    end
 end
 
-function table.GetWinningKey(t) 	 -- Inversed logics are sometimes faster. Trust me.
-	local out, max = next(t)
-
-	for k, v in pairs(t) do
-		if v < out then continue end 
-		out, max = k, v
-	end
-
-	return out
+function table.GetWinningKey(t)
+    local max_val, max_key
+    for k, v in pairs(t) do
+        if not max_val or v > max_val then
+            max_val = v
+            max_key = k
+        end
+    end
+    return max_key
 end

76857902a9b64b45f68d9917a6415f7e96303082
pockets <33330490+pocketssss@users.noreply.github.com>
2023-02-19 05:21:28 +0300

    Update vector.lua

--- simple_math/lua/libs/vector.lua
+++ simple_math/lua/libs/vector.lua
@@ -1,31 +1,32 @@
 local vec = FindMetaTable("Vector")
 
 local min, max = math.min, math.max
+local sqrt = math.sqrt
 
-function vec:Length()		 -- Maths are faster than functions.
+function vec:Length()
 	local x, y, z = self.x, self.y, self.z
-	return (x * x + y * y + z * z) ^ 0.5
+	return sqrt(x * x + y * y + z * z)
 end
 
-function vec:Distance(pos)	 -- Same as top.
+function vec:Distance(pos)
 	local v = self - pos
 	local x, y, z = v.x, v.y, v.z
-	return (x * x + y * y + z * z) ^ 0.5
+	return sqrt(x * x + y * y + z * z)
 end
 
-function vec.Normalize(v)	 -- No conditions. Should be faster.
+function vec.Normalize(v)
 	local l = v:Length()
 	v.x = v.x / l
 	v.y = v.y / l
 	v.z = v.z / l
 end
 
-function vec:GetNormalized()	 -- Same as top.
+function vec:GetNormalized()
 	local l = self:Length()
 	local x, y, z = self.x, self.y, self.z
 	return Vector(x, y, z) / l
 end
 
-function vec.Dot(a, b)			-- Sometimes I tend to think that everything in Garry's Mod is broken.
-	return a.x * b.x + a.y * b.y + a.z * b.y
+function vec.Dot(a, b)
+	return a.x * b.x + a.y * b.y + a.z * b.z
 end

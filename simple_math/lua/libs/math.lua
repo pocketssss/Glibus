3f3e609e23814ba6f5ef8f80055f1128b21ee72e
techno atomic tangerine <sumpilot@gmail.com>
2023-03-02 20:19:36 +0300

    Update math.lua
    - The stigma of shame is washed away

--- simple_math/lua/libs/math.lua
+++ simple_math/lua/libs/math.lua
@@ -1,70 +1,60 @@
 -- Some constants
 local doublepi, halfpi, pi = math.pi * 2, math.pi * 0.5, math.pi
-
--- Cached math functions
-local floor = math.floor
-local sqrt = math.sqrt
-
--- Cached table functions
-local tableSort = table.sort
-
--- Cached random function
-local sharedRandom = math.SharedRandom
-
--- Precomputed coefficients for sine approximation
-local a = -0.255
-local b = 1.27323954
-local c = 0.405284735
-
--- Precompute the values for sin and cos
-local sinValues = {}
-local cosValues = {}
-local sinfValues = {}
-local cosfValues = {}
-
-for i = 0, 360 do
-  sinValues[i] = math.sin(i)
-  cosValues[i] = math.cos(i)
-  sinfValues[i] = math.sinf(i)
-  cosfValues[i] = math.cosf(i)
-end
-
--- Optimized functions
+local select, fmod = select, math.fmod
 
 function math.rad(n)
-  return n * pi / 180
+    return n * pi / 180
 end
 
-function math.deg(n)
-  return n * 180 / pi
+local function max(max, ...)
+    local select = select
+
+    for i = 1, select('#', ...) do
+        local v = select(i, ...)
+        max = (max < v) and v or max
+    end
+
+    return max
 end
 
-function math.min(...)
-  local t = {...}
-  tableSort(t)
-  return t[1]
+local function min(min, ...)
+    local select = select
+
+    for i = 1, select('#', ...) do
+        local v = select(i, ...)
+        min = (min > v) and v or min
+    end
+
+    return min
 end
 
-function math.max(...)
-  local t = {...}
-  tableSort(t, function(a, b) return a > b end)
-  return t[1]
-end
-
-function math.SharedRandom(l, h)
-  l = l or 0
-  h = h or l or 0xFFFFFF
-  return floor(sharedRandom() * (h - l + 1) + l)
-end
+math.min, math.max = min, max
 
 function math.Clamp(n, l, h)
-  return n < l and l or (n > h and h or n)
+    return min(max(n, l), h)
 end
 
-local a, b, c = -0.166667, 0.00833, -0.000198
+math.oldrandom = math.random
 
-function math.sin_cos(n)
+do
+    local x, m = 1, 2 ^ 32
+
+    function math.randomseed(n)
+        x = tonumber(n)
+    end
+
+    function math.random(lo, hi)
+        x = (135335 * x + 1337) % m
+        if lo and hi then return lo + ((x - lo - 1) % hi) end
+
+        return x / m
+    end
+end
+
+-- Dude, if you're using work that has lost its value over 5 years ago and mention me, then don't make me shame.
+function math.sin(n)
     n = n % doublepi
+
     if n < -pi then
         n = n + doublepi
     elseif n > pi then
@@ -72,38 +62,43 @@ function math.sin_cos(n)
     end
 
     local x2 = n * n
-    local sin = n * (1 + x2 * (a + x2 * b))
-    local cos = 1 + x2 * (a + x2 * c)
 
-    return sin, cos
-end
-
-function math.sin(n)
-    return math.sin_cos(n)
+    return n * (1 + x2 * (-.166667 + x2 * .00833))
 end
 
 function math.cos(n)
-    _, cos = math.sin_cos(n)
-    return cos
+    n = n % doublepi
+
+    if n < -pi then
+        n = n + doublepi
+    elseif n > pi then
+        n = n - doublepi
+    end
+
+    local x2 = n * n
+
+    return 1 + x2 * (-.166667 + x2 * -.000198)
 end
 
---Same, but not so accurate.
-function math.sinf(n)
-	local n = n % doublepi
-	
-	if n < -pi then
-		n = n + doublepi
-	elseif n > pi then
-		n = n - doublepi
-	end
+-- Quadric curve sinus
+local function qsin(n)
+    n = fmod(n, doublepi)
 
-	if n < 0 then
-		return n * (1.27323954 + 0.405284735 * n)
-	else
-		return n * ( 1.27323954 - 0.405284735 * n)
-	end
+    if n < -pi then
+        n = n + doublepi
+    elseif n > pi then
+        n = n - doublepi
+    end
+
+    if n < 0 then
+        return n * (1.27323954 + .405284735 * n)
+    else
+        return n * (1.27323954 - .405284735 * n)
+    end
 end
 
-function math.cosf(n)
-	return math.sinf(n + halfpi)
+local function qcos(n)
+    return qsin(n + halfpi)
 end
+
+math.sinf, math.cosf = qsin, qcos

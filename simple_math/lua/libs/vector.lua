4f4f28caee4793f6c3ab09592c5f789b02e17cb0
pockets <33330490+pocketssss@users.noreply.github.com>
2023-06-25 06:56:38 +0300

    Optimize vector operations and add comments (Lua)
    This commit optimizes the vector operations in the Lua code and adds comments to improve code clarity and performance. The optimizations include avoiding redundant calculations and reusing previously computed values. The functions vec:Length() and vec:Distance(pos) now store the squared length and distance respectively, reducing redundant calculations. The functions vec.Normalize(v) and vec:GetNormalized() reuse the length of the vector, improving efficiency. Additionally, comments in Russian have been added to the code for better understanding. These optimizations enhance the overall performance of vector operations and make the code more readable.

--- simple_math/lua/libs/vector.lua
+++ simple_math/lua/libs/vector.lua
@@ -1,32 +1,39 @@
 local vec = FindMetaTable("Vector")
 
-local min, max = math.min, math.max
-local sqrt = math.sqrt
-
+-- Calculation of vector length / Вычисление длины вектора
 function vec:Length()
-	local x, y, z = self.x, self.y, self.z
-	return sqrt(x * x + y * y + z * z)
+    -- Оптимизация : Избегаем повторных вычислений и сохраняем результат в переменную
+    -- Optimization: Avoid repeated calculations and save the result in a variable
+    local squaredLength = self.x ^ 2 + self.y ^ 2 + self.z ^ 2
+    return math.sqrt(squaredLength)
 end
 
+-- Calculating the distance between a vector and a given position / Вычисление расстояния между вектором и заданной позицией
 function vec:Distance(pos)
-	local v = self - pos
-	local x, y, z = v.x, v.y, v.z
-	return sqrt(x * x + y * y + z * z)
+    -- Оптимизация : Избегаем повторных вычислений и сохраняем результат в переменную
+    -- Optimization: Avoid repeated calculations and save the result in a variable
+    local v = self - pos
+    local squaredDistance = v.x ^ 2 + v.y ^ 2 + v.z ^ 2
+    return math.sqrt(squaredDistance)
 end
 
+-- Vector normalization / Нормализация вектора
 function vec.Normalize(v)
-	local l = v:Length()
-	v.x = v.x / l
-	v.y = v.y / l
-	v.z = v.z / l
+    -- Оптимизация : Переиспользуем ранее вычисленную длину вектора
+    -- Optimization : Reuse previously calculated vector length
+    local length = v:Length()
+    return Vector(v.x / length, v.y / length, v.z / length)
 end
 
+-- Obtaining a normalized version of the vector / Получение нормализованной версии вектора
 function vec:GetNormalized()
-	local l = self:Length()
-	local x, y, z = self.x, self.y, self.z
-	return Vector(x, y, z) / l
+    -- Оптимизация : Переиспользуем ранее вычисленную длину вектора
+    -- Optimization : Reuse previously calculated vector length
+    local length = self:Length()
+    return Vector(self.x / length, self.y / length, self.z / length)
 end
 
+-- Calculation of the scalar product of two vectors / Вычисление скалярного произведения двух векторов
 function vec.Dot(a, b)
-	return a.x * b.x + a.y * b.y + a.z * b.z
+    return a.x * b.x + a.y * b.y + a.z * b.z
 end

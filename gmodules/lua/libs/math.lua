84e1e88e54179a664e8b69aa8a3b62a5ee734216
pockets <33330490+pocketssss@users.noreply.github.com>
2023-06-25 13:37:38 +0300

    Update math.lua

--- gmodules/lua/libs/math.lua
+++ gmodules/lua/libs/math.lua
@@ -3,8 +3,6 @@ local select, fmod = select, math.fmod
 local a = -0.255
 local b = 1.27323954
 local c = 0.405284735
-local select = select
-local fmod = math.fmod
 
 function math.rad(n)
   return n * pi / 180

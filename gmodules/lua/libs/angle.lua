5a56b2be36e2d20a73b233feeaaac6760a0ec7da
pockets <33330490+pocketssss@users.noreply.github.com>
2023-06-25 11:19:14 +0300

    Update angle.lua

--- gmodules/lua/libs/angle.lua
+++ gmodules/lua/libs/angle.lua
@@ -1,9 +1,6 @@
 local ang = FindMetaTable("Angle")
-
--- Caching math library functions / Кэширование функций математической библиотеки
 local sin, cos, atan2 = math.sin, math.cos, math.atan2
 
--- Optimized function for obtaining a vector from an angle / Оптимизированная функция получения вектора из угла
 function ang:Forward()
     local pitch, yaw = self.p, self.y
     local p = sin(pitch)
@@ -12,7 +9,6 @@ function ang:Forward()
     return Vector(y, r, p)
 end
 
--- Optimized gaze direction vector acquisition function / Оптимизированная функция получения вектора направления взгляда
 function ang:Up()
     local pitch, yaw, roll = self.p, self.y, self.r
     local p = -sin(pitch) * cos(roll)
@@ -21,7 +17,6 @@ function ang:Up()
     return Vector(y, r, p)
 end
 
--- Optimized function for obtaining the lateral displacement vector / Оптимизированная функция получения вектора бокового смещения
 function ang:Right()
     local pitch, yaw, roll = self.p, self.y, self.r
     local p = sin(pitch) * sin(roll)
